92a23b7f737ebbc070a349c22f38a93594f01f1a
Aromal S <aromalsanthosh@protonmail.com>
2021-12-20 22:58:40 +0530

    styling changes

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Nav.css'
 //import bootstrap navbar with search bar
 import { Navbar, Nav,  Button , Container} from 'react-bootstrap';
-
+import {FaSearch} from 'react-icons/fa'
 
 function NavBar() {
     return (
@@ -10,20 +10,22 @@ function NavBar() {
             <Navbar  bg="light" expand="lg">
                 <Container fluid>
                     <div className='searchbar'>
-                        <form class="form-search form-inline">
-                            <input type="text" class="search-query" placeholder="Search..." />
-                        </form>
+                        
+                        <div className="box">
+                            <FaSearch className='search-icon'/>
+                            <input type="text" className='search-query' placeholder="Search..." />
+                        </div>
                     </div>
 
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                     
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#courses">Courses</Nav.Link>
+                    <Nav className="ms-auto nav-items">
+                        <Nav.Link id="nav-link" href="#home">Home</Nav.Link>
+                        <Nav.Link id="nav-link1" href="#courses">Courses</Nav.Link>
                     </Nav>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart m-3" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-cart m-3" viewBox="0 0 16 16">
                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                     </svg>
 
